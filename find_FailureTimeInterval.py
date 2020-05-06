d40af4b547ed54d8ab3ee5978364e4c329237a92
17617 <1761711567@qq.com>
2020-05-06 12:25:48 +0800

    3sigma

--- find_FailureTimeInterval.py
+++ find_FailureTimeInterval.py
@@ -2,6 +2,8 @@ import csv
 import os
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
+
 import data_path
 from read_data import readCsvWithPandas
 n=3
@@ -20,3 +22,9 @@ def failureTimeInterval():
     threeSigma(timeStamp,avg_time,succee_rate)
 if __name__ == '__main__':
     failureTimeInterval()
+    res2=readCsvWithPandas(path)
+    res=res2[:,2]
+    # 建立MinMaxScaler对象
+    minmax = preprocessing.MinMaxScaler()
+    # 标准化处理
+    
