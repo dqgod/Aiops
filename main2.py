c42f9e3fd5ffe265884b98084aaee632a08791a3
dq <1319468035@qq.com>
2020-05-09 13:32:22 +0800

    main2.py

---
+++ main2.py
@@ -0,0 +1,218 @@
+#%%
+import numpy as np
+import os
+import sys
+import re
+import json
+import matplotlib.pyplot as plt
+import data_path  # 路径
+import data_cleaning
+from read_data import readCSV, read_xlrd,readCsvWithPandas
+from datetime import datetime
+from xlrd import xldate_as_tuple
+from show_Kpis import getKpis
+kpi_opened = {}
+##是否是执行者调用   
+isExecutor = {"JDBC":False,"LOCAL":False,"CSF":False,"FlyRemote":True,"OSB":True,"RemoteProcess":True}
+#%%
+def find_abnormal_indicators(execption_Interval, cmdb_id):
+    """[该时间区间内那个指标错误]
+
+    Args:
+        execption_Interval ([type]): [时间区间]
+        cmdb_id ([type]): [网源]
+    """
+    kpis = None
+    abnormal_indicators = []
+    # os,docker,db
+    file_name = data_path.fileNames[cmdb_id.split('_')[0]]
+    # file_path = os.path.join(data_path.get_data_path(),"平台指标",file_name)
+    # 查看当前文件是否已经分解
+    if kpi_opened.get(file_name) == None:
+        kpis = getKpis([file_name])
+        kpi_opened[file_name] = kpis
+    else:
+        kpis = kpi_opened[file_name]
+    # 逐个指标的进行判断
+    for k, v in kpis.items():
+        temp = k.split(',')  # (cmdb_id,name,bomc_id,itemid)
+        if cmdb_id == temp[0]:
+            # todo 进行异常评估，给出得分
+            score = anomaly_detection(execption_Interval, v)
+            abnormal_indicators.append((temp[0], temp[1], temp[2], score))
+    # 排序返回得分最高的三个
+    return sorted(abnormal_indicators, key=lambda x: x[3], reversed=True)[:3]
+
+def anomaly_detection(execption_Interval, data):
+    """[异常检测算法]
+
+    Args:
+        execption_Interval ([tutle]): [时间区间(start-time,end-time)]
+        data ([type]): [description]
+    """
+    pass
+
+
+def find_abnormal_span(trace):
+    """按照图的遍历方式遍历trace中的所有span\n
+    Args:
+        trace ([dict]): 一条trace，格式{ startTime:str,{spanId:{},spanId:{}}}        \n
+    Returns:
+        [list]: 返回异常节点       \n
+    """
+    spans = trace['spans']
+    graph = data_cleaning.generateGraph(spans)
+    abnormal_cmdb_ids = []
+    net_fault = 0
+    Break = True
+    # isError代表上溯的节点是否有异常
+    def traverse(root_id,abn_ids,isError=False):
+        root = spans[root_id]
+        if isExecutor[root['callType']] and root['callType']!='OSB' and \
+            float(root['duration'])>1000:
+            abn_ids.append(root["cmdb_id"])
+            net_fault += 1
+        # 如果上溯有异常或本身有异常
+        if isError or root['success'] == 'False':
+            if isExecutor[root['callType']] and root['callType']!='OSB':
+                abn_ids.append(root["cmdb_id"])
+            # 当发现是数据库出现问题时，将其他的清空，只保存数据库cmdb_id,并退出递归
+            if root['db'] and root['success'] == 'False':
+                abn_ids.clear()
+                abn_ids.append(root["db"])
+                return Break
+            isError = True
+        # 如果没有子节点，直接返回
+        if graph.get(root_id) == None:
+            return not Break
+        for span_id in graph[root_id]:
+            if traverse(span_id,abn_ids,isError)==Break:
+                return Break
+        return not Break
+
+    for root_id in graph['root']:
+        abn_ids = []
+        traverse(root_id,abn_ids)
+        abnormal_cmdb_ids += abn_ids
+    return list(set(abnormal_cmdb_ids))
+
+def find_abnormal_trace(execption_Interval, traces):
+    """找到改异常区间内所有trace
+
+    Args:
+        execption_Interval ([type]): [时间区间]
+        traces ([type]): [description]
+    """
+    abnormal_trace = []
+    for trace in traces.values():
+        startTime = int(trace['startTime'])
+        if startTime > execption_Interval[0] and startTime < execption_Interval[1]:
+            abnormal_trace.append(trace)
+    return abnormal_trace
+
+
+def fault_time(bias=0):
+    table = read_xlrd(os.path.join(
+        data_path.get_data_path(), "数据说明", "0故障内容.xlsx"))
+    table_head = table.row_values(0)
+    time_index, duration_index = 0, 0
+    for i in range(table.ncols):
+        if table_head[i] == 'time':
+            time_index = i
+        elif table_head[i] == 'duration':
+            duration_index = i
+    res = []
+    for i in range(1, table.nrows):
+        row = table.row_values(i)
+        cell = table.cell_value(i, time_index)
+        date = datetime(*xldate_as_tuple(cell, 0))
+        # print(date)
+        time_stamp = int(datetime.timestamp(date))*1000
+        duration = int(re.match('\d*', row[duration_index])[0])*60*1000
+        res.append((time_stamp, time_stamp+duration))
+    return res
+
+
+def find_time_interval(data):
+    """
+    在入口服务中，找到异常的数据的时间戳
+    """
+    avg_times = data[:, 2].astype(np.float32)
+    succee_rate = data[:, 5].astype(np.float32)
+    std = np.std(avg_times, ddof=1)
+    means = np.mean(avg_times)
+    high = means+3*std
+    # low = means-3*std
+    def lam(i): return avg_times[i] > high or succee_rate[i] < 1
+    res = data[list(filter(lam,  range(len(data))))]
+    return res[:, 1].astype(np.int64)
+
+
+def to_period_time(timestamps, bias=5*1000):
+    '''
+    将时间戳的序列转换为时间段
+    '''
+    r = list(map(lambda t: (t-bias, t+bias), timestamps))
+    return r
+
+
+def draw(data):
+    x = range(len(data))
+    plt.figure(figsize=(8, 5))
+    # 获取平均处理时间一列
+    y = data[:, 2].astype(np.float32)
+    plt.subplot(2, 1, 1)
+    # plt.title("平均调用时间")
+    plt.plot(x, y, label="平均调用时间")
+
+    # 获取成功率一列
+    y = data[:, 5].astype(np.float32)
+    plt.subplot(2, 1, 2)
+    # plt.title("成功率")
+    plt.plot(x, y, color='r', label="成功率")
+
+    plt.show()
+
+# %%
+# 业务指标
+business_path = os.path.join(data_path.get_data_path(), "业务指标", "esb.csv")
+# 调用链指标,平台指标,数据说明
+trace_p,plat_p,data_instruction_p = data_cleaning.getPath()
+# 获取业务指标数据，去掉表头
+data = readCsvWithPandas(business_path)
+# todo step1 异常时间序列
+execption_times = find_time_interval(data)
+
+# todo step2 异常时间区间
+# period_times = to_period_time(execption_times)
+period_times = fault_time()
+#%%
+# todo step3 获取所有trace
+traces = data_cleaning.build_trace(trace_p)
+
+#%%
+# trace0 = list(traces.values())[0]
+# g = data_cleaning.generateGraph(trace0['spans'])
+# with open('graph.json','w') as f:
+#     js = json.dumps(g,indent=4)
+#     f.write(js)
+# todo step4 找出这段时间内的trace
+for i,execption_Interval in enumerate(period_times):
+    # execption_Interval = (0,15865349796310)
+    abnormal_traces = find_abnormal_trace(execption_Interval, traces)
+    # print(len(abnormal_traces))
+    abnormal_cmdb_ids = []
+    # todo step5 找出异常数据中的异常节点
+    for trace in abnormal_traces:
+        #! 以下未实现
+        abnormal_cmdb_ids +=find_abnormal_span(trace)
+    abnormal_cmdb_ids = list(set(abnormal_cmdb_ids))
+    print(i+1,abnormal_cmdb_ids)
+    # todo step6 判断该节点是哪个指标有异常
+    # for cmdb_id in abnormal_cmdb_ids:
+    #     # ? 找到异常指标
+    #     abnormal_indicators = find_abnormal_indicators(
+    #         execption_Interval, cmdb_id)
+      
+    
+# %%
